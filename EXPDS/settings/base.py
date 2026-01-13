f322af72132ec2d0ec76cc65abae6c58016f21e6
Achmad Irfan <achmad.irfan1@yahoo.com>
2026-01-13 18:05:29 +0700

    setting prod

--- EXPDS/settings/base.py
+++ EXPDS/settings/base.py
@@ -12,21 +12,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4i%$tgpdi_s1((b68^0d4t)59b8=igz7w+)#)kbe=v9+hza3t4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,16 +74,7 @@ WSGI_APPLICATION = 'EXPDS.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'expedition',    
-        'USER': 'root',        
-        'PASSWORD': 'dave', 
-        'HOST': 'localhost',     
-        'PORT': '3306'
-        }          
-}
+
 
 
 # Password validation
@@ -121,8 +111,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
